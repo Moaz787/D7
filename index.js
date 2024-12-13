590522cd5b631857a596be0fb79dd8fcb298fbfc
Moaz <148714017+Moaz787@users.noreply.github.com>
2024-12-13 11:35:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 mongoose
   .connect(process.env.DB)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server is running on port 3000");
     });
     console.log("Connected to MongoDB");
